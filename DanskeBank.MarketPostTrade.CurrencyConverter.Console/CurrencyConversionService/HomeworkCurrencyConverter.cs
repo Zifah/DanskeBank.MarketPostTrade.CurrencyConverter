4620f23b24b25ab7d6572316b1fe235d097ce5e7
Hafiz Adewuyi <hoadewuyi@gmail.com>
2023-12-12 09:40:11 +0100

    Create CurrencyConverter interface

---
+++ DanskeBank.MarketPostTrade.CurrencyConverter.Console/CurrencyConversionService/HomeworkCurrencyConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DanskeBank.MarketPostTrade.CurrencyConverter.Console.CurrencyConversionService
+{
+    internal class HomeworkCurrencyConverter : ICurrencyConverter
+    {
+        public decimal Convert(string currencyPair, decimal amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DanskeBank.MarketPostTrade.CurrencyConverter.Console/CurrencyConversionService/ICurrencyConverter.cs
@@ -0,0 +1,18 @@
+﻿namespace DanskeBank.MarketPostTrade.CurrencyConverter.Console.CurrencyConversionService
+{
+    internal interface ICurrencyConverter
+    {
+        /// <summary>
+        /// Converts the <paramref name="amount"/> from the MainCurrency to the MoneyCurrency
+        /// </summary>
+        /// <param name="currencyPair">Should be in the format: MainCurrency/MoneyCurrency (case insensitive)</param>
+        /// <param name="amount">The amount in the MainCurrency to be converted to </param>
+        /// <returns>The result of the currency conversion.</returns>
+        /// <exception cref="ArgumentException">
+        /// This exception is thrown in two cases:
+        /// 1. The currency pair is not valid
+        /// 2. No exchange rates were found for the currency pair
+        /// </exception>
+        decimal Convert(string currencyPair, decimal amount);
+    }
+}
