f3dbe1045735cfb7d7591ebec9ca8fb72b5ea28c
Hafiz Adewuyi <hoadewuyi@gmail.com>
2023-12-12 09:45:57 +0100

    Restructure projects

---
+++ Application/HomeworkCurrencyConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application;
+
+internal class HomeworkCurrencyConverter : ICurrencyConverter
+{
+    public decimal Convert(string currencyPair, decimal amount)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Application/ICurrencyConverter.cs
@@ -0,0 +1,17 @@
+﻿namespace Application;
+
+internal interface ICurrencyConverter
+{
+    /// <summary>
+    /// Converts the <paramref name="amount"/> from the MainCurrency to the MoneyCurrency
+    /// </summary>
+    /// <param name="currencyPair">Should be in the format: MainCurrency/MoneyCurrency (case insensitive)</param>
+    /// <param name="amount">The amount in the MainCurrency to be converted to </param>
+    /// <returns>The result of the currency conversion.</returns>
+    /// <exception cref="ArgumentException">
+    /// This exception is thrown in two cases:
+    /// 1. The currency pair is not valid
+    /// 2. No exchange rates were found for the currency pair
+    /// </exception>
+    decimal Convert(string currencyPair, decimal amount);
+}
